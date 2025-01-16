06f0eae4b8617de84b87dfae1dac178205bf8b57
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-16 18:24:39 +0100

    update

--- E_API_Key.py
+++ E_API_Key.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
 api_key = st.text_input("Clée API")
-st.session_state["api_key"] = api_key
+if api_key:
+    st.session_state["api_key"] = api_key
--- E_App.py
+++ E_App.py
@@ -3,3 +3,4 @@ import streamlit as st
 pg = st.navigation([st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_API_Key.py", title ="Configuration de la clée API")])
 
 pg.run()
+st.sidebar("Aide au devoirs", "Créateur de fiches de révision", "Configuration de la clée API")
