cf17fdecc14a733f121b80b4feb4d096d96e7aef
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-16 18:11:03 +0100

    Update E_API_Key.py

--- E_API_Key.py
+++ E_API_Key.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.write("Pour utilisier l'IA EtudIAnt, tu dois rendre sur le site de google")
-st.link_button("https://aistudio.google.com/app/u/2/apikey")
 st.write("Une fois connecté à Google, appuis sur le bouton créer une clée API")
 st.write("Reviens ensuite sur cette page et rentre ta clée api ici :")
 
