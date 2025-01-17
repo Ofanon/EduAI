872bd6ff2316faefd5938681384314867b0f75d1
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-17 14:33:10 +0100

    Update E_H.py

--- E_H.py
+++ E_H.py
@@ -47,8 +47,8 @@ if "image_analyzed" in st.session_state:
 if "chat_history" in st.session_state:
     for message in st.session_state["chat_history"]:
         if message["role"] == "user": 
-            st.chat_message('user')
-            st.write(f"**Vous** : {message['content']}")
+            message_user = st.chat_message('user')
+            message_user.write(f"**Vous** : {message['content']}")
         elif message["role"] == "assistant":
-            st.chat_message('assistant')
-            st.write(f"**IA** : {message['content']}")
+            message_ai = st.chat_message('assistant')
+            message_ai.write(f"**IA** : {message['content']}")
