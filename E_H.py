2956ae077a4bfe43517c2a64dd410a8179835362
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-17 14:14:11 +0100

    Update E_H.py

--- E_H.py
+++ E_H.py
@@ -47,6 +47,6 @@ if "image_analyzed" in st.session_state:
 if "chat_history" in st.session_state:
     for message in st.session_state["chat_history"]:
         if message["role"] == "user": 
-            st.chat_message(st.session_state["content"])
+            st.dialog(f"**Vous** : {message['content']}")
         elif message["role"] == "assistant":
             st.write(f"**IA** : {message['content']}")
