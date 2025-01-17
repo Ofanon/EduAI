8feea858bb54edbbb6be76005e75d60506d5fe2b
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-17 14:03:53 +0100

    Update E_API_Key.py

--- E_API_Key.py
+++ E_API_Key.py
@@ -8,7 +8,7 @@ def generate_api_key():
     api_key = st.text_input("Entre ta clée API ici.")
     if st.button("Enregistrer la clée API"):
         if api_key:
-            st.session_state["api_key"] =  api_key
+            st.session_state["api_key"] = api_key
     return api_key
 
 def save_api_key(user_id, api_key):
