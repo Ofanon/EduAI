9ad296a8ab15962222d10f1eb464e7b5dc9fd7bb
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-17 18:41:02 +0100

    update

--- E_R.py
+++ E_R.py
@@ -42,15 +42,15 @@ if "created" in st.session_state:
         st.session_state["chat_add"].append({"role":"assistant", "content":response_chat.text})
         history.append({"role":"user", "parts":prompt_user})
         history.append({"role":"model", "parts":response_chat.text})
-
-if "chat_add" in st.session_state:
-    for message in st.session_state["chat_add"]:
-        if message["role"] == "user":
-            message_user = st.chat_message('user')
-            message_user.write(f"**Vous** : {message['content']}")
-        elif message["role"] == "assistant":
-            message_ai = st.chat_message('assistant')
-            message_ai.write(f"**AI** : {message['content']}")
+if "created" in st.session_state:
+    if "chat_add" in st.session_state:
+        for message in st.session_state["chat_add"]:
+            if message["role"] == "user":
+                message_user = st.chat_message('user')
+                message_user.write(f"**Vous** : {message['content']}")
+            elif message["role"] == "assistant":
+                message_ai = st.chat_message('assistant')
+                message_ai.write(f"**AI** : {message['content']}")
         
     
 
