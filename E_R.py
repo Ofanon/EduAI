71164b4ee2e73c3feb8063f3cd4ab34c4e1e3797
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-18 14:19:18 +0100

    up

--- E_R.py
+++ E_R.py
@@ -54,7 +54,7 @@ if "created" in st.session_state:
 if "chat_add" in st.session_state:
     for message in st.session_state["chat_add"]:
         if message["role"] == "user":
-            message_user = st.chat_message('user').
+            message_user = st.chat_message('user')
             message_user.write(f"**Vous** : {message['content']}")
         elif message["role"] == "assistant":
             message_ai = st.chat_message('assistant')
