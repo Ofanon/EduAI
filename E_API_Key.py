f52d8535b0db382f73409c15da1de99989afbeb0
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-18 17:29:33 +0100

    update

--- E_API_Key.py
+++ E_API_Key.py
@@ -86,6 +86,7 @@ elif st.session_state["action"] == "Se connecter":
             st.error("Identifiant ou mot de passe incorrect.")
     if st.button("Pas de compte ? En créer un"):
         st.session_state["action"] = "Créer un compte"
+        st.experimental_rerun()
 
 if st.session_state["authenticated"] == True:
     st.subheader("Votre clé API")
