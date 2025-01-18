41a138ba3e0102d2e6d3045e50848b9f5da5440b
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-18 16:42:39 +0100

    update

--- E_R.py
+++ E_R.py
@@ -9,6 +9,7 @@ if "api_key" in st.session_state:
     genai.configure(api_key=st.session_state["api_key"])
 else:
     st.error("Clée API non enregistrée, veuillez vous rendre dans l'onglet 'Connexion à l'EtudIAnt' pour l'enregistrer.")
+st.write(st.session_state["api_key"])
 
 if "chat_add" not in st.session_state:
     st.session_state["chat_add"] = []
