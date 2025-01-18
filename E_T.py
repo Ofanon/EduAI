ad5c7393dba63f7cdee4339c0342aaca22982587
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-18 19:27:16 +0100

    sdq

--- E_T.py
+++ E_T.py
@@ -15,11 +15,20 @@ st.title("EtudIAnt : Créateur de contrôles")
 if "analyze_image_finished" not in st.session_state:
     st.session_state["analyze_image_finished"] = False
 
-uploaded_file = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
 
 if st.button("Créer un contrôle sur ce cours"):
     if "api_key" in st.session_state:
-        for file in uploaded_file:
+        images = None
+        for file in uploaded_files:
             image = Image.open(file)
-            st.image(image, use_column_width=True)
+            st.image(image, use_container_width=True)
+            images.append(image)
+        if st.session_state["analyze_image_finished"]:
+            prompt = "Crée un contrôle sur ces images. Le contrôle doit contenir differents types de questions."
+            with st.spinner("L'EtudIAnt reflechit..."):
+                response = model.generate_content([images, prompt])
+                response_user = response.text
+                st.write(response_user)
+            
 
