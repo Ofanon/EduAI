f9f18b448fe5e9f23458170e06ae84d6f9591c23
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-19 19:59:03 +0100

    Update E_R.py

--- E_R.py
+++ E_R.py
@@ -21,8 +21,8 @@ model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 st.subheader("Sur quoi veux-tu créer une fiche de révision ?")
 level = st.selectbox('Séléctionne ton niveau : ', ["6ème","5ème","4ème","3ème","Seconde","Premiere","Terminale"])
-prompt = "Crée une fiche de revision le plus précisement possible. La fiche de revision doit être niveau :" + level + "En parlant francais, jamais en anglais"
-
+subject = st.selectbox('Séléctionne la matière de ta fiche de révision:', ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol"])
+prompt = "Crée une fiche de revision le plus précisement possible. La fiche de revision doit être au niveau :" + level + "Adapte la fiche de révision en fonction du niveau"+ "Cette fiche de revision est sur la matière:"+subject+"En parlant francais, jamais en anglais"
 prompt_user = st.chat_input("ex : sur la seconde guerre mondiale.")
 
 if prompt_user:
