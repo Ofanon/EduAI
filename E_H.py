23404377f0394bec4c2dc7061ecab8ef408cd652
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-19 16:42:13 +0100

    Update E_H.py

--- E_H.py
+++ E_H.py
@@ -40,9 +40,9 @@ if uploaded_file:
             st.session_state["st_image"] = st_image
             if "image_analyzed" not in st.session_state:
                 prompt = "Répond à cette exercice le plus précisement possible. En parlant en francais, jamais en anglais"
-                response_ai = model.generate_content([prompt, image])
-                response_ai_user = response_ai.text
                 with st.spinner("L'EtudIAnt reflechit..."):
+                    response_ai = model.generate_content([prompt, image])
+                    response_ai_user = response_ai.text
                     st.session_state["response_ai"] = response_ai_user
                     st.session_state["chat_history"].append({"role":"assistant","content":response_ai.text})
                     st.session_state["image_analyzed"] = True
