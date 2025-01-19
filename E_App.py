d9db364671f3c9ba7ff48fa3836fa58849374a0d
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-19 17:00:01 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("E_API_Key.py", title ="Connexion à l'EtudIAnt"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision")])
+pg = st.navigation([st.Page("E_API_Key.py", title ="Connexion à l'EtudIAnt"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
 
 pg.run()
