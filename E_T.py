6f1f5611af7310038c4298e0bc14f12b9ab017e1
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-21 19:10:32 +0100

    Update E_T.py

--- E_T.py
+++ E_T.py
@@ -25,8 +25,6 @@ if st.session_state["user_informations"] == []:
 user_infos = st.session_state["user_informations"]
 if st.session_state["user_informations"] == []: 
     level = st.selectbox('Sélectionne ton niveau : ', ["6ème","5ème","4ème","3ème","Seconde","Premiere","Terminale"])
-else:
-    level = user_infos["level"]
     
 prompt = f"Voici un groupe d'images d'un cours de niveau {level}. Crée un contrôle comme au college ou lycée dessus en adaptant la difficultée en fonction du niveau er de la matière : {subject}. Répond en parlant francais, jamais en anglais. Ne fais pas ton introduction dans la réponse, fais directement le contrôle"
 
