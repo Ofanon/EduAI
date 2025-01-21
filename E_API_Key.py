1187625dac34cd99649355b1b3ea7e203d1f0e91
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-21 18:44:04 +0100

    Update E_API_Key.py

--- E_API_Key.py
+++ E_API_Key.py
@@ -148,9 +148,10 @@ if "connected" in st.session_state:
     st.title(f"Vous êtes connecté !")
     st.success(f"Clé API existante : {st.session_state["api_key"]}")
     st.session_state["authenticated"] = True
-    st.subheader("Plus d'infos sur toi !")
-    st.write("Répond au formulaire pour adapter l'EtudIAnt à tes besoins.")
-    st.selectbox("Sélectionne ton niveau :", ["6ème", "5ème", "4ème", "3ème", "Seconde", "Première", "Terminale"])
-    st.selectbox("Quel matière préfères-tu ?", ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol", "Aucune matière"])
-    st.slider("Definis ton niveau en cours :", 0, 100)
+    with st.form(key="user_info_form"):
+        st.subheader("Plus d'infos sur toi !")
+        st.write("Répond au formulaire pour adapter l'EtudIAnt à tes besoins.")
+        st.selectbox("Sélectionne ton niveau :", ["6ème", "5ème", "4ème", "3ème", "Seconde", "Première", "Terminale"])
+        st.selectbox("Quel matière préfères-tu ?", ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol", "Aucune matière"])
+        st.slider("Definis ton niveau en cours :", 0, 100)
 
