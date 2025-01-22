5c8cc1240388735577c25448bdb6e6385db25edd
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-22 21:24:21 +0100

    Update E_Quiz.py

--- E_Quiz.py
+++ E_Quiz.py
@@ -22,9 +22,9 @@ subject = st.selectbox("Sélectionne la matière du quiz :", ["Français", "Math
 def get_question():
     response_ai =  model.generate_content([f"Créer un quiz de niveau juste pour une question {level}, et dans la matière {subject} avec 4 choix de réponses pour une correcte. Tu dois parler en français pas en anglais. Crée la réponse comme un container json qui contient : question, choices, correct_answer, explanation."])
     st.write(response_ai.text)
-
+    response_ai_json = r'response_ai.text'
     try:
-        data = json.loads(response_ai.text)
+        data = json.loads(response_ai_json)
     except json.decoder.JSONDecodeError as e:
         st.error(f"Erreur de décodage JSON : {e}")
         return {}
