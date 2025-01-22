7d14fc17404f1e5514dd464337ebc2f897c8e487
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-22 16:09:15 +0100

    Update E_T.py

--- E_T.py
+++ E_T.py
@@ -21,9 +21,9 @@ if st.session_state["user_informations"] == []:
     level = st.selectbox('Sélectionne ton niveau : ', ["6ème","5ème","4ème","3ème","Seconde","Premiere","Terminale"])
 
 level = st.selectbox('Sélectionne ton niveau : ', ["6ème","5ème","4ème","3ème","Seconde","Premiere","Terminale"])
-difficulty = st.slider("Defini la difficultée du contrôle de 1 à 10 :", 1, 10)
 subject = st.selectbox('Sélectionne la matière de ta fiche de révision:', ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol"])   
-prompt = f"Voici un groupe d'images d'un cours de niveau {level}. Crée un contrôle comme au college ou lycée dessus en adaptant la difficultée en fonction du niveau er de la matière : {subject}. Répond en parlant francais, jamais en anglais. Ne fais pas ton introduction dans la réponse, fais directement le contrôle"
+difficulty = st.slider("Defini la difficultée du contrôle de 1 à 10 :", 1, 10)
+prompt = f"Voici un groupe d'images d'un cours de niveau {level}. Crée un contrôle de difficulté : {difficulty} sur 10. Comme au college ou lycée dessus en adaptant la difficultée en fonction du niveau er de la matière : {subject}. Répond en parlant francais, jamais en anglais. Ne fais pas ton introduction dans la réponse, fais directement le contrôle"
 uploaded_files = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
 
 def display_images(files):
