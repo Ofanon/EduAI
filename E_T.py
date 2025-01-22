86ad857b54d3d67114c9614f4752bcd566c3b0bc
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-22 16:07:52 +0100

    Update E_T.py

--- E_T.py
+++ E_T.py
@@ -9,13 +9,12 @@ if "api_key" in st.session_state:
 else:
     st.error("Clé API non enregistrée, veuillez vous rendre dans l'onglet 'Connexion à l'EtudIAnt' pour l'enregistrer.")
 
+model = genai.GenerativeModel(model_name="gemini-1.5-flash-002")
+
 if "analyze_image_finished" not in st.session_state:
     st.session_state["analyze_image_finished"] = False
 if "chat_control" not in st.session_state:
     st.session_state["chat_control"] = []
-model = genai.GenerativeModel(model_name="gemini-1.5-flash-002")
-if "response_pdf" not in st.session_state:
-    st.session_state["response_pdf"] = None
 
 st.title("EtudIAnt : Créateur de contrôles")
 if st.session_state["user_informations"] == []: 
@@ -23,9 +22,8 @@ if st.session_state["user_informations"] == []:
 
 level = st.selectbox('Sélectionne ton niveau : ', ["6ème","5ème","4ème","3ème","Seconde","Premiere","Terminale"])
 difficulty = st.slider("Defini la difficultée du contrôle de 1 à 10 :", 1, 10)
-    
+subject = st.selectbox('Sélectionne la matière de ta fiche de révision:', ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol"])   
 prompt = f"Voici un groupe d'images d'un cours de niveau {level}. Crée un contrôle comme au college ou lycée dessus en adaptant la difficultée en fonction du niveau er de la matière : {subject}. Répond en parlant francais, jamais en anglais. Ne fais pas ton introduction dans la réponse, fais directement le contrôle"
-
 uploaded_files = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
 
 def display_images(files):
@@ -46,7 +44,6 @@ if uploaded_files:
                     with st.spinner("L'EtudIAnt reflechit..."):
                         place_holder_button.empty()
                         response = model.generate_content([prompt]+ images)
-                        st.session_state["response_pdf"] = response
                     st.session_state["chat_control"].append({"role": "assistant", "content": response.text})
     else:
         st.error("Veuillez enregistrer votre clé API pour utiliser l'EtudIAnt.")
