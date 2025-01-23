c5605a31daff7ae5df005580758f0face126f8eb
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-23 14:25:06 +0100

    Update E_Quiz.py

--- E_Quiz.py
+++ E_Quiz.py
@@ -15,9 +15,20 @@ if "question_count" not in st.session_state:
 def get_question():
     model = genai.GenerativeModel("gemini-1.5-flash-002")
     with st.spinner("Le quiz est en cours de création"):
-        response = model.generate_content(f"Crée un QCM d'une question de niveau {level} et de matière {subject}, avec 4 réponses pour une réponse correcte. La réponse doit etre en format text decodable par JSON comme un container JSON avec : question, choices, correct_answer, explanation ")
-        data = json.loads(response.text)
-        return data
+        response = model.generate_content(f"Crée un QCM d'une question de niveau {level} et de matière {subject}, avec 4 réponses pour une réponse correcte. La réponse doit etre en format text, pas code, decodable par JSON avec : question, choices, correct_answer, explanation ")
+    st.write(response.text)
+    data = json.loads({
+  "question": "Quel mot est un synonyme de \"joie\" ?",
+  "choices": [
+    "tristesse",
+    "bonheur",
+    "colère",
+    "peur"
+  ],
+  "correct_answer": "bonheur",
+  "explanation": "Le mot \"bonheur\" exprime un sentiment de satisfaction et de contentement, tout comme le mot \"joie\"."
+})
+    return data
 
 if st.button("Commencer le quiz"):
     st.session_state["data"] = get_question()
