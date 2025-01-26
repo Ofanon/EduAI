4085f3c31e4506b3d0fab8c835ad5e0d13cfdee8
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-26 17:28:13 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -1,15 +1,16 @@
 import streamlit as st
 
 logo = "logo.png"
-st.set_page_config(
-    page_title="EtudIAnt",
-    page_icon= logo,
-    initial_sidebar_state="auto",
-    menu_items={
-        'Get Help': None,
-        'Report a bug': None,
-        'About': None
+
+st.markdown(
+    """
+    <style>
+    .viewerBadge_container__1QSob { 
+        display: none !important; 
     }
+    </style>
+    """,
+    unsafe_allow_html=True
 )
 pg = st.navigation([st.Page("E_API_Key.py", title ="Connexion à l'EtudIAnt"),st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
 
