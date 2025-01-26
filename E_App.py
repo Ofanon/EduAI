eb3260cc02e051faa7523917b133208e37b21975
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-26 17:32:37 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -2,12 +2,6 @@ import streamlit as st
 
 logo = "logo.png"
 
-hide_github_icon = """
-#GithubIcon {
-  visibility: hidden;
-}
-"""
-st.markdown(hide_github_icon, unsafe_allow_html=True)
 pg = st.navigation([st.Page("E_API_Key.py", title ="Connexion à l'EtudIAnt"),st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
 
 pg.run()
