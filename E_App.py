7eef8958ae71c02e586a8e8468c8819e89ec3f54
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-27 19:37:06 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -7,7 +7,7 @@ with st.sidebar():
     st.write(f"Etoiles restantes : {db_manager.get_requests_left()}")
 logo = "logo.png"
 
-pg = st.navigation([st.Page("E_API_Key.py", title ="Connexion à l'EtudIAnt"),st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
-with st.sidebar():
-    pg.run()
+pg = st.navigation([st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
+
+pg.run()
 
