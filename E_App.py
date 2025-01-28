37eaae03ee69875017947dba50ccfb276bcf48ab
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-28 17:29:18 +0100

    update

--- E_App.py
+++ E_App.py
@@ -2,10 +2,11 @@ import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
 import data.db_manager as db_manager
+import hydralit_components as hc
 
 with st.sidebar:
     st.write(f"⭐ Etoiles restantes : {db_manager.get_requests_left()}")
-    pg = st.navigation([st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
+    pg = hc.nav_bar([st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
 
 pg.run()
 
---
+++ E_Shop.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import hydralit_components as hc
+
+st.title("Boutique de l'EtudIAnt")
+
+hc.b
