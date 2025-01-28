36bcee40911464d0a557238476329b9a7795a034
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-28 09:23:50 +0100

    Update E_T.py

--- E_T.py
+++ E_T.py
@@ -13,6 +13,7 @@ if "started" not in st.session_state:
     st.session_state["analyze_image_finished"] = False
     st.session_state["chat_control"] = []
     st.session_state.response_download = None
+    st.session_state.uploaded_files = None
     st.session_state.started = False
 
 st.title("EtudIAnt : Créateur de contrôles")
@@ -22,7 +23,7 @@ if st.session_state.started == False:
     subject = st.selectbox('Sélectionne la matière de ta fiche de révision:', ["Français", "Mathématiques", "Histoire-Géographie-EMC", "Sciences et Vie de la Terre", "Physique Chimie", "Anglais","Allemand", "Espagnol"])   
     difficulty = st.slider("Définis la difficultée du contrôle de 1 à 10 :", 1, 10)
     prompt = f"Voici un groupe d'images d'un cours de niveau {level}. Crée un contrôle de difficulté : {difficulty} sur 10. Comme au college ou lycée dessus en adaptant la difficultée en fonction du niveau er de la matière : {subject}. Répond en parlant francais, jamais en anglais. Ne fais pas ton introduction dans la réponse, fais directement le contrôle"
-    uploaded_files = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
+    st.session_state.uploaded_files = st.file_uploader("Télécharge les photos de tes cours.", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=True)
 
 def display_images(files):
     images = []
@@ -58,10 +59,10 @@ def create_dynamic_word_doc(response):
 
 place_holder_button = st.empty()
 
-if uploaded_files and len(uploaded_files) > 0 or st.session_state.started == True:
+if st.session_state.uploaded_files and len(st.session_state.uploaded_files) > 0 or st.session_state.started == True:
     if place_holder_button.button("Créer un contrôle sur ce cours"):
         if db_manager.can_user_make_request():
-            images = display_images(uploaded_files)
+            images = display_images(st.session_state.uploaded_files)
             if not st.session_state["analyze_image_finished"]:
                 with st.spinner("L'EtudIAnt reflechit..."):
                     place_holder_button.empty()
