d96d94a419e92dc0be69153a40b6bfbdaa2f4ebe
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-28 19:46:55 +0100

    Update E_Quiz.py

--- E_Quiz.py
+++ E_Quiz.py
@@ -121,11 +121,12 @@ if "started" in st.session_state:
             if st.session_state.points == None:
                 st.session_state.points = st.session_state.note * 10
                 db.update_experience_points(points=st.session_state.points)
+                st.rerun()
+                st.write(db.get_experience_points())
             st.success(f"Vous avez gagné {st.session_state.points} points d'experience !")
             st.balloons()
             if st.button("Refaire un autre quiz"):
                 del st.session_state.started
                 st.session_state.can_start = False
-                st.rerun()
 
 
