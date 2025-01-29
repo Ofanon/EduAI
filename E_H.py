6de533add91d24f3e278b91f07d0f8c8c8cb7f28
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 17:52:35 +0100

    Update E_H.py

--- E_H.py
+++ E_H.py
@@ -4,7 +4,7 @@ import PIL
 import streamlit as st
 import db_manager as db_manager
 
-st.title("📚 EtudIAnt : Aide au devoirs")
+st.title("EtudIAnt : 📚 Aide au devoirs")
 genai.configure(api_key=st.secrets["API_KEY"])
 
 model = genai.GenerativeModel(model_name="gemini-1.5-flash-002")
