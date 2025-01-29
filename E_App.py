d91e2d8e9938956e5aafb36a58887ede3f9ebd93
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 16:17:25 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -3,8 +3,22 @@ from streamlit_lottie import st_lottie
 import requests
 import db_manager as db
 import uuid
+
 if "user_id" not in st.session_state:
     st.session_state["user_id"] = str(uuid.uuid4())
+
+import streamlit as st
+import db_manager
+
+# Vérifie si un ID existe déjà, sinon crée-le
+if "user_id" not in st.session_state:
+    st.session_state["user_id"] = db_manager.get_user_id()
+
+st.write(f"👤 ID utilisateur : `{st.session_state['user_id']}`")
+st.write(f"⭐ Requêtes restantes : {db_manager.get_requests_left()}")
+st.write(f"💰 Points d'expérience : {db_manager.get_experience_points()}")
+
+
 with st.sidebar:
     st.write(f"⭐ Etoiles restantes : {db.get_requests_left()}")
     pg = st.navigation([st.Page("E_Shop.py", title="Boutique"),st.Page("E_Quiz.py", title = "Quiz interactif"), st.Page("E_H.py", title = "Aide aux devoirs"), st.Page("E_R.py", title = "Créateur de fiches de révision"), st.Page("E_T.py", title= "Créateur de contrôle")])
