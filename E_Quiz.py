f8c3ebe4842a4e5d75b9773350c861fed80cda72
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 16:30:09 +0100

    Update E_Quiz.py

--- E_Quiz.py
+++ E_Quiz.py
@@ -39,7 +39,7 @@ with st.spinner("La page est en cours de chargement..."):
         st.session_state.difficulty = None
         st.session_state.user_prompt = None
         st.session_state.current_question = None
-        st.session_state.question_count = 1
+        st.session_state.question_count = 0
         st.session_state.started = False
         st.session_state.data = None
         st.session_state.question = None
