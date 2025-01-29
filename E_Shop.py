dcc6bb837b3c44ef698232d42e0180910c83291d
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 18:22:15 +0100

    update

--- E_Shop.py
+++ E_Shop.py
@@ -5,19 +5,13 @@ experience_points = db.get_experience_points()
 
 st.title("🌟 Boutique de l'EtudIAnt 🌟")
 st.subheader(f"💎 Vous avez **{experience_points}** points d'expérience.")
-with st.container():
-    st.image("star.png", width=100)
-    st.markdown("### ⭐ Pack 5 étoiles")
-    st.write("💎 **1000 XP**")
-    if st.button("🛒 Acheter"):
-        st.success("✅ 5 étoiles ajoutées !")
+
 st.markdown("---")
 
 col1, col2, col3 = st.columns([1, 1, 1])
 
 with col1:
-    st.image("star (2).png", width=110)
-    st.write("⭐ Acheter 5 étoiles")
+    st.image("images/5_stars.png", width=110)
     if st.button("🎁 **Prix** : 💎 1000 points d'expérience", key="buy_5"):
         success = db.purchase_requests(cost_in_experience=1000, requests_to_add=5)
         if success:
@@ -28,8 +22,7 @@ with col1:
             st.error("❌ Pas assez de points.")
 
 with col2:
-    st.image("branding.png", width=110)
-    st.write("⭐⭐ Acheter 10 étoiles")
+    st.image("images/10_stars.png", width=110)
     if st.button("🎁 **Prix** : 💎 2800 points d'expérience", key="buy_10"):
         success = db.purchase_requests(cost_in_experience=2800, requests_to_add=10)
         if success:
@@ -40,8 +33,7 @@ with col2:
             st.error("❌ Pas assez de points.")
 
 with col3:
-    st.image("star.png", width=110)
-    st.write("⭐⭐⭐ Acheter 20 étoiles")
+    st.image("images/20_stars.png", width=110)
     if st.button("🎁 **Prix** : 💎 5700 points d'expérience", key="buy_20"):
         success = db.purchase_requests(cost_in_experience=5700, requests_to_add=20)
         if success:
