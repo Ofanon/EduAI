095f4468694cf10c0c06edc9e27f4b17c4660ed4
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 21:54:15 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -4,9 +4,8 @@ import requests
 import db_manager as db
 import uuid
 
-with st.sidebar:
-    st.write(f"⭐ Etoiles restantes : {db.get_requests_left()}")
-    pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle")])
+st.write(f"⭐ Etoiles restantes : {db.get_requests_left()}")
+pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle")])
 
 pg.run()
 
