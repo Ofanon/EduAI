d345f7d16796e0f7c63b15b5f41651ea34bd22cd
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-29 12:00:47 +0100

    Update E_Shop.py

--- E_Shop.py
+++ E_Shop.py
@@ -11,7 +11,7 @@ st.markdown("---")
 col1, col2, col3 = st.columns([1, 1, 1])
 
 with col1:
-    st.image("star (2).png", width=150)
+    st.image("star (2).png", width=110)
     st.write("⭐ Acheter 5 étoiles")
     if st.button("🎁 **Prix** : 💎 200 points d'expérience", key="buy_5"):
         success = db.purchase_requests(cost_in_experience=200, requests_to_add=5)
@@ -22,7 +22,7 @@ with col1:
             st.error("❌ Pas assez de points.")
 
 with col2:
-    st.image("branding.png", width=150)
+    st.image("branding.png", width=110)
     st.write("⭐⭐ Acheter 10 étoiles")
     if st.button("🎁 **Prix** : 💎 380 points d'expérience", key="buy_10"):
         success = db.purchase_requests(cost_in_experience=380, requests_to_add=10)
@@ -33,7 +33,7 @@ with col2:
             st.error("❌ Pas assez de points.")
 
 with col3:
-    st.image("star.png", width=150)
+    st.image("star.png", width=110)
     st.write("⭐⭐⭐ Acheter 20 étoiles")
     if st.button("🎁 **Prix** : 💎 720 points d'expérience", key="buy_20"):
         success = db.purchase_requests(cost_in_experience=720, requests_to_add=20)
