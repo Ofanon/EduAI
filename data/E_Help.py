132737e74fa30611dbc88d57678b1e758039349a
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-30 19:10:54 +0100

    Create E_Help.py

---
+++ data/E_Help.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("EtudIAnt : Aide")
+
+st.subheader("1 : Les points d'experience")
+
+st.write("Les points d'experience sont gagnés en fonction des actions réalisées dans l'application. Ils permettent de suivre la progression et de débloquer certains avantages au fil du temps.")
+st.write("Vous gagnez des points d'experience en participant aux différents services proposés par l'EtudIAnt.")
+
+
+st.subheader("2 : Les étoiles")
+
+st.write("Les étoiles correspondent aux demandes que vous pouvez faire à l'IA. Chaque fois que vous utilisez un service de l'EtudIAnt, vous consommez 1 étoile.")
+st.write("Plus d'étoile ? Pas de panique ! Vous pouvez en acheter dans la boutique grâce à vos points d'experience de plus vous recevez 5 étoiles tous les jours, si il ne vous en reste plus !")
