e4ff70081e12eda5b9a93c7694ccecb5560afa8d
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-30 19:15:40 +0100

    Update E_Help.py

--- data/E_Help.py
+++ data/E_Help.py
@@ -7,7 +7,6 @@ st.subheader("1 : 💎 Les points d'experience")
 st.write("Les points d'experience 💎 sont la monnaie de l'EtudIAnt.")
 st.write("Vous gagnez des points d'experience en participant aux différents services proposés par l'EtudIAnt.")
 
-
 st.subheader("2 : ⭐ Les étoiles")
 
 st.write("Les étoiles ⭐ correspondent aux demandes que vous pouvez faire à l'IA. Chaque fois que vous utilisez un service de l'EtudIAnt, vous consommez 1 étoile.")
