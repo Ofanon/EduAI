aedb72e41a5e667aefe531e2dcfcb18673a1f752
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-01-30 19:16:01 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -4,7 +4,7 @@ import db_manager as db
 
 with st.sidebar:
     st.write(f"⭐ Etoiles restantes : {db.get_requests_left()}")
-    pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle"), st.Page("E_Help.py", title="⭐💎 Aide")])
+    pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle"),st.Page("E_Help.py", title="⭐💎 Aide")])
 
 pg.run()
 
