6da3f495ce4262215ae465e5171b0e424abb9dff
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-01 23:03:37 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -85,7 +85,6 @@ if "user_confirmed" in st.session_state and st.session_state["first_name"]:
             st.error("Vous n'avez plus de requêtes disponibles.")
 
 with st.sidebar:
-    st.write(f"⭐ Etoiles restantes : {db_manager.get_requests_left()}")
     pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle"), st.Page("E_Help.py", title= "⭐💎 Aide")])
 
 pg.run()
