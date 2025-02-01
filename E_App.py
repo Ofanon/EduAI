a6c4b87b81258b75ea2e4f54d29c23a2668ccb0a
Ofanon <100065775+Ofanon@users.noreply.github.com>
2025-02-01 22:19:44 +0100

    Update E_App.py

--- E_App.py
+++ E_App.py
@@ -3,9 +3,9 @@ from streamlit_lottie import st_lottie
 import db_manager as db
 import sqlite3
 import streamlit as st
-import db_manager
 
-st.write(db.get_user_id)
+st.write(f"votre id : {db.get_user_id}")
+
 with st.sidebar:
     st.write(f"⭐ Etoiles restantes : {db.get_requests_left()}")
     pg = st.navigation([st.Page("E_Shop.py", title="🛒 Boutique"),st.Page("E_Quiz.py", title = "🎯 Quiz interactif"), st.Page("E_H.py", title = "📚 Aide aux devoirs"), st.Page("E_R.py", title = "📒 Créateur de fiches de révision"), st.Page("E_T.py", title= "📝 Créateur de contrôle"), st.Page("E_Help.py", title= "⭐💎 Aide")])
